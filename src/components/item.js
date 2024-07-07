548ee1ad09d3a726eea3dc291c36e80398b30708
Andrey Stepanenko <astelth17@gmail.com>
2024-07-07 22:36:09 +0300

    fix

--- src/components/item.js
+++ src/components/item.js
@@ -1,23 +1,31 @@
 import React from "react";
 import Image from "./image";
-import { IoCloseCircleSharp, IoHammerSharp } from "react-icons/io5";
 import { FaArrowRightLong } from "react-icons/fa6";
 import styles from './item.module.css';
-
+import cn from 'classnames';
 
 class Item extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     item = this.props.item;
     render() {
         return (
-            <div className={styles.item}>
-                <FaArrowRightLong className={styles.arrowIcon} />
-                <IoHammerSharp className='edit-icon' />
-                <h3 className="user-name">{this.item.title} {this.item.subtitle}</h3>
-                <p>{this.item.description}</p>
-                <Image image={this.item.image} className={styles.image} />
+            <div className={cn(styles.item, { [styles.itemHeader]: this.item.text })}>
+                <div className={styles.head}>
+                    <div className={styles.title}>
+                        {this.item.title}
+                    </div>
+                    <div className={styles.text}>
+                        {this.item.text}
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div>
+                        <Image image={this.item.image} className={cn(styles.image, { [styles.no]: this.item.text })} />
+                    </div>
+                    <div className={styles.subtitle}>
+                        {this.item.subtitle}
+                        <FaArrowRightLong className={styles.arrowIcon} />
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/list.js
+++ src/components/list.js
@@ -3,22 +3,10 @@ import Item from "./item";
 import styles from './list.module.css';
 
 class List extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         if (this.props.list.length > 0)
             return (
                 <div className={styles.wrapper}>
-                    <div className={styles.item}>
-                        <div className={styles.title}>
-                            ИНДИВИДУАЛЬНОЕ ИЗГОТОВЛЕНИЕ ШТОР
-                        </div>
-                        <div className={styles.description}>
-                            8 видов систем, более 1000 видов тканей, и множество дополнительных возможностей 
-                            позволяют создать реально уникальную и уютную атмосферу в вашем доме
-                        </div>
-                    </div>
                     {this.props.list.map((el) => (
                         <Item key={el.id} item={el} />
                     ))}
