2eb2a4022cad8ea27700afd11f0d033ff2fd62df
Andrey Stepanenko <astelth17@gmail.com>
2024-07-16 00:13:47 +0300

    fix

--- src/components/list.js
+++ src/components/list.js
@@ -8,11 +8,13 @@ class List extends React.Component {
         if (this.props.list.length > 0)
             return (
                 <div className={styles.wrapper}>
-                    <Subhead title='ИНДИВИДУАЛЬНОЕ ИЗГОТОВЛЕНИЕ ШТОР' isCentered isGridA
-                        text=' 8 видов систем, более 1000 видов тканей, и множество дополнительных возможностей позволяют создать реально уникальную и уютную атмосферу в вашем доме' />
-                    {this.props.list.map((el) => (
-                        <Item key={el.id} item={el} />
-                    ))}
+                    <div className={styles.inner}>
+                        <Subhead title='ИНДИВИДУАЛЬНОЕ ИЗГОТОВЛЕНИЕ ШТОР' isCentered isGridA
+                            text=' 8 видов систем, более 1000 видов тканей, и множество дополнительных возможностей позволяют создать реально уникальную и уютную атмосферу в вашем доме' />
+                        {this.props.list.map((el) => (
+                            <Item key={el.id} item={el} />
+                        ))}
+                    </div>
                 </div>
             )
         else
